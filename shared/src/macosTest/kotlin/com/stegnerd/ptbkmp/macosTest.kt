804979cdff60425ce9a89f389d99f107f94644af
Dalton Stegner <dmstegner@gmail.com>
2022-06-04 17:45:38 -0400

    trigger change for mac test

--- shared/src/macosTest/kotlin/com/stegnerd/ptbkmp/macosTest.kt
+++ shared/src/macosTest/kotlin/com/stegnerd/ptbkmp/macosTest.kt
@@ -5,7 +5,6 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class  MacosGreetingTest {
-
     @Test
     fun testExample() {
         assertTrue(Greeting().greeting().contains("macOS"), "Check macOS is mentioned")
